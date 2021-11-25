0606760f8b831b19e40e55b3c4685dd31580bd96
Strange-bs <80538114+Strange-bs@users.noreply.github.com>
2021-11-25 22:10:52 +0500

    Intro

---
+++ lessson_1_introduction/manage.py
@@ -0,0 +1,15 @@
+var = "Hello world"
+print(var)
+if var:
+    print(True)
+else:
+    print(False)
+
+List = ['any', 'data', True, 6, 'type', 'can', {'find': 'here'}]
+Dict = {
+    'name': "Strange",
+    'surname': "Stephan",
+    'isMarried': False,
+    'language': "javaScript"
+}
+print(List)
