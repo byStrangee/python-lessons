44ed98a61e5653d52692e5c95180a9699630a695
Strange-bs <qosimovrahmatullo006@gmail.com>
2022-03-07 14:58:35 +0000

    :heavy_plus_sign:

--- app/_firebase.py
+++ app/_firebase.py
@@ -1,2 +1,3 @@
 from config import auth, db
 from utils import write_data, get_data, sign_up, sign_in
+
--- app/utils.py
+++ app/utils.py
@@ -1,21 +1,34 @@
 from config import auth, db
-
+import json
 def sign_up(email, password):
     try:
         auth.create_user_with_email_and_password(email, password)
-        print('succesfully registered')
+        print('succesfully registered')    
+        return True
     except:
         print('Error with connection')
-
+        return False
 
 def write_data(table, values):
     try:
         db.child(table).set(values)
+        return True
     except Exception as err:
         print(err)
+        return False
 
 def get_data(table):
     pass
 
 def sign_in(email, password):
-    pass
+    try:
+        auth.sign_in_with_email_and_password(email, password)
+        print('Succesfully logged in')
+        return True
+    except Exception as err:
+        _err = json.loads(err.strerror)
+        _errno = err.errno
+        _errmessage = _err['error']['message']
+        print(_errno)
+        print(_errmessage)
+        return {"err": _errmessage, "errno": _errno}
